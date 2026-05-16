89255f1d52e67c4e242686a687ddbb9d052b0a8e
prasad3131 <prasad.nice@gmail.com>
2026-05-15 22:27:47 -0500

    increase json body limit to 50mb for base64 photo uploads

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const photosRouter = require('./routes/photos');
 const setupRouter = require('./routes/setup');
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 app.use('/photos', photosRouter);
 app.use('/setup', setupRouter);
